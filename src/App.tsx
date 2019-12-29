75fa055bce2e1b31dcfaa8fa1108f9093692c2a6
HuyTD <thewish.it@gmail.com>
2019-12-29 07:49:01 +0700

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      Have fun!
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/CommonComponents/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import routes from "./routes.constants";
+
+const CommonComponents = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default CommonComponents;
